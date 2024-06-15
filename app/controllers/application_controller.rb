739e4a2f68649662f13dd8ec0297380f227ce8ed
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-06-15 09:45:27 +0000

    public_registrations1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :telephone_number, :address])
   end
 end
