e9df084ac2e4eee4fe09204dfa776fbfb292a302
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-06-15 09:33:31 +0000

    public_registrations

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,9 @@ ActiveRecord::Schema.define(version: 2024_06_14_143534) do
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
@@ -80,9 +83,6 @@ ActiveRecord::Schema.define(version: 2024_06_14_143534) do
     t.string "address", null: false
     t.string "telephone_number", null: false
     t.boolean "is_active", default: true, null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
