14f4187784b3c2efc5f2b4d274ac8ba966217d7a
yuta369 <y.m.36912@gmail.com>
2024-06-15 09:28:19 +0000

    [Add] public-items

--- config/routes.rb
+++ config/routes.rb
@@ -51,16 +51,21 @@ Rails.application.routes.draw do
 
   # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+
+# devise_for :customers,skip: [:passwords], controllers: {
+#   registrations: "public/registrations",
+#   sessions: 'public/sessions'
+# }
+  devise_for :customers
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+
+# devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+#   sessions: "admin/sessions"
+# }
+
+  devise_for :admin
 # root to の後のコントローラをpublicに指定してある↓
   root to: 'public/homes#top'
   get 'about' => 'public/homes#about'
