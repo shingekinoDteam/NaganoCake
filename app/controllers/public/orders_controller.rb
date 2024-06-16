0f5c40c5060a8176f969c9c3cd6efceadc802ef4
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-16 11:22:17 +0000

    [Add]作業途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,21 +1,70 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
   def new
+    @order = Order.new
+  end
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+
+    if @order.save
+       cart_items.each do |cart|
+          order_detail = OrderItem.new
+          order_detail.item_id = cart.item_id
+          order_detail.order_id = @order.id
+          order_detail.amount = cart.amount
+
+          order_detail.price = cart.item.price
+          order_item.save
+       end
+       redirect_to orders_thanks_path
+       cart_items.destroy_all
+    else
+       @order = Order.new(order_params)
+       render :new
+    end
   end
 
   def confirm
-
+    @order = Order.new(order_params)
+     if params[:order][:address_option] == "0"
+       @order.name = current_customer.name
+       @order.address = current_customer.customer_address
+     elsif params[:order][:address_option] == "1"
+       if Address.exists?(name: params[:order][:registered])
+           @order.name = Address.find(params[:order][:registered]).name
+           @order.address = Address.find(params[:order][:registered]).address
+       else
+       render :new
+       end
+     elsif params[:order][:address_option] == "2" 
+       address_new = current_customer.addresses.new(address_params)
+       if address_new.save
+       else
+         render :new
+  　    end
+  　 end
+  　 #cart-itemモデルで定義def sum_price item.taxin_price*quantity end
+  　 #@cart_items = current_customer.cart_items.all 
+    # @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
   end
-
+  
   def thanks
   end
 
-  def create
-
-  end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:postal_code,:address,:name,:payment_method,:shipping_cost,:total_payment,:status,:customer_id)
+  end
+
+  def address_params
+  params.require(:order).permit(:name, :address, :postal_code)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :customer
+
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
