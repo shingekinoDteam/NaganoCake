73219fb0909849d852cefa19282f4428a0fde6d9
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-17 04:00:59 +0000

    [Add] 注文画面作業途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,62 +1,65 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
+
   def new
     @order = Order.new
+    @addresses = current_customer.addresses.all
   end
+
   def create
-    cart_items = current_customer.cart_items.all
-    @order = current_customer.orders.new(order_params)
+    order = Order.new(order_params)
+    order.save
+    @cart_items = current_customer.cart_items.all
 
-    if @order.save
-       cart_items.each do |cart|
-          order_detail = OrderItem.new
-          order_detail.item_id = cart.item_id
-          order_detail.order_id = @order.id
-          order_detail.amount = cart.amount
-
-          order_detail.price = cart.item.price
-          order_item.save
-       end
-       redirect_to orders_thanks_path
-       cart_items.destroy_all
-    else
-       @order = Order.new(order_params)
-       render :new
+    @cart_items.each do |cart_item|
+      @order_details = OrderDetail.new
+      @order_details.order_id = order.id
+      @order_details.item_id = cart_item.item.id
+      @order_details.price = cart_item.item.price_excluding_tax
+      @order_details.amount = cart_item.amount
+      @order_details.making_status = 0
+      @order_details.save!
     end
+
+    CartItem.destroy_all
+    redirect_to orders_thanks_path
+
   end
 
   def confirm
     @order = Order.new(order_params)
+    @addresses = current_customer.addresses.all
+
      if params[:order][:address_option] == "0"
-       @order.name = current_customer.name
+       @order.postal_code = current_customer.postal_code
+       @order.name = current_customer.first_name + current_customer.last_name
        @order.address = current_customer.customer_address
+
      elsif params[:order][:address_option] == "1"
-       if Address.exists?(name: params[:order][:registered])
-           @order.name = Address.find(params[:order][:registered]).name
-           @order.address = Address.find(params[:order][:registered]).address
-       else
-       render :new
-       end
-     elsif params[:order][:address_option] == "2" 
-       address_new = current_customer.addresses.new(address_params)
-       if address_new.save
-       else
-         render :new
-  　    end
-  　 end
-  　 #cart-itemモデルで定義def sum_price item.taxin_price*quantity end
-  　 #@cart_items = current_customer.cart_items.all 
-    # @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
+       @address = Address.find(params[:order][:address_id])
+       @order.postal_code = @address.postal_code
+       @order.address = @address.address
+       @order.name = @address.name
+
+     elsif params[:order][:address_option] == "2"
+       @order.customer_id = current_customer.id
+     end
+       @cart_items = current_customer.cart_items
+       @order_new = Order.new
+       render :confirm
   end
-  
+
   def thanks
   end
 
 
   def index
+     @orders = Order.all
   end
 
   def show
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order = Order.find(params[:id])
   end
 
   private
@@ -64,7 +67,7 @@ class Public::OrdersController < ApplicationController
     params.require(:order).permit(:postal_code,:address,:name,:payment_method,:shipping_cost,:total_payment,:status,:customer_id)
   end
 
-  def address_params
-  params.require(:order).permit(:name, :address, :postal_code)
-  end
+　def price_excluding_tax
+      price * 1.1
+　end
 end
