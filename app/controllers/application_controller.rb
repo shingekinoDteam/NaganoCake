b3b70c6b938bcb3145945c705295649091ce2980
Monica <167148997+Monicawang4949@users.noreply.github.com>
2024-06-18 05:01:25 +0000

    admin_sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    about_path
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_path
+    else
+      root_path
+    end
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+      root_path
+    elsif resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   protected
