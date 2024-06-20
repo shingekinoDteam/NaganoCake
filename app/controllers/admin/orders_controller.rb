5fa6d3d4c0501675e99a16cc0955ddd78ea6cbd1
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-20 06:40:42 +0000

    [Update]admin-orders　showが正しく表示されるように修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order_detail =  OrderDetail.find(params[:id])
     @order = Order.find(params[:id])
+    @order_details = OrderDetail.all
   end
 
    def update
