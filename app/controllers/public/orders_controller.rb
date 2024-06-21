7a2b3bb1df996d4825afc869d44747379fcf7221
suzukinanae55018 <165752907+suzukinanae55018@users.noreply.github.com>
2024-06-21 05:24:24 +0000

    [Fix] cart_itemsの削除を限定

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -24,7 +24,7 @@ class Public::OrdersController < ApplicationController
       end
       flash[:notice] = "注文が成功しました"
 
-      CartItem.destroy_all
+      current_customer.cart_items.destroy_all
       redirect_to orders_thanks_path
     else
       flash.now[:alert] = "注文に失敗しました"
@@ -39,7 +39,7 @@ class Public::OrdersController < ApplicationController
     @order.payment_method = params[:order][:payment_method]
      if params[:order][:address_option] == "0"
        @order.postal_code = current_customer.postal_code
-       @order.name = current_customer.first_name + current_customer.last_name
+       @order.name = current_customer.last_name + current_customer.first_name
        @order.address = current_customer.address
 
      elsif params[:order][:address_option] == "1"
@@ -50,6 +50,9 @@ class Public::OrdersController < ApplicationController
 
      elsif params[:order][:address_option] == "2"
        @order.customer_id = current_customer.id
+       @order.postal_code = params[:order][:postal_code]
+       @order.address = params[:order][:address]
+       @order.name = params[:order][:name]
      end
        @cart_items = current_customer.cart_items
        @order_new = Order.new
